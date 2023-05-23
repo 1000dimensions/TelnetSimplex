1a076b0e327eab908dfa94d11dfd76af0e6fc5b9
Who Knows <75544834+1000dimensions@users.noreply.github.com>
2023-05-23 09:07:54 -0400

    Server

---
+++ WIPS.py
@@ -0,0 +1,53 @@
+import socket
+from threading import Thread
+import threading
+import os
+HOST = "10.101.129.17"
+##HOST = "192.168.1.123"
+##HOST = socket.gethostbyname(socket.gethostname()) 
+print(socket.gethostname())
+PORT = 8080
+clients = set()
+clientLock = threading.Lock()
+def on_new_client(cs, addr):
+    cs.send(b"What is the username? ")
+    username = cs.recv(1024).decode()
+    cs.send(b"""Welcome to SimplexChat!
+To quit just type in quit()
+Have fun communcating!
+""")
+    spam == 0
+    while True:
+        data = cs.recv(1024).decode()
+        if data == "":
+            print("User has put in a blank")
+            spam = spam + 1
+            if spam == 5:
+                cs.send(b"""You have put in a blank 5 times.
+Due to our spam policy we must disconnect you.
+You are able to reconnect.
+Good Bye.""")
+                break
+        if data == "quit()":
+            break
+        with clientLock:
+            data = "User:" + username + "> " + data
+            for c in clients:
+                c.sendall(data.encode())
+    cs.close()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+
+s.listen()
+while True:
+    conn, addr = s.accept()
+    with clientLock:
+        clients.add(conn)
+    print("New connection from {addr}" )
+    thread = Thread(target=on_new_client, args=(conn, addr))
+    thread.start()
+    
+conn.close()
+thread.join()        
+
+
